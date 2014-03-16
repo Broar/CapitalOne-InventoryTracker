b23f205647272b1dd1e8d842de215a673258e54e
Steven Briggs <gshbriggs@gmail.com>
2014-03-16 00:58:37 -0400

    Removed the custom exception. Added new functions.
    I decided the RecordDoesNotExistError was unnecessary in this project.
    If the sqlite3 module doesn't include exceptions for when reading or
    updating non-existent records, there's probably a good reason for it.
    
    I added readProductName and readID functions. These will help us to
    determine the existence of products and their IDs in the database.
    
    Finally, I decided to change the coding style to camel-case instead of
    the traditional underscores. This decision was purely personal
    preference.

--- python/inventory.py
+++ python/inventory.py
@@ -11,27 +11,6 @@ logging.basicConfig(
      level=logging.INFO,
      format='%(levelname)s:%(asctime)s:%(message)s')
 
-class RecordDoesNotExistError(Exception):
-    """
-    This error should raised when database operations on non-existent 
-    records are performed
-    """
-    def __init__(self, msg):
-        """
-        Initialize the RecordDoesNotExistError
-        
-        @param msg:  A message detailing the error
-        """
-        self.msg = msg
-        
-    def __str__(self):
-        """
-        A string representation of the RecordDoesNotExistError
-        
-        @return:  A message detailing the error
-        """
-        return repr(self.msg)
-
 class InventoryDatabase(object):
     """
     Creates a SQLite3 database of products and their unique identifiers.
@@ -42,9 +21,9 @@ class InventoryDatabase(object):
         
         @__db:  Database containing products and their IDs
         """
-        self.__db = self.establish_connection()
+        self.__db = self.openConnection()
         
-    def establish_connection(self):
+    def openConnection(self):
         """
         Establish a connection with the product database
         
@@ -52,19 +31,19 @@ class InventoryDatabase(object):
         """
         return sqlite3.connect(DB_NAME)
     
-    def close_connection(self):
+    def closeConnection(self):
         """
         Close the connection with the product database
         """
         self.__db.close()
     
-    def create_inventory(self):
+    def createInventory(self):
         """
         Create a table in the database called 'inventory' with the columns
-        id (primary key), product, and code.
+        index (primary key), name, and ID.
         """
         try:
-            self.__db.execute("CREATE TABLE inventory(id INTEGER PRIMARY KEY, product TEXT UNIQUE, code TEXT UNIQUE)")
+            self.__db.execute("CREATE TABLE inventory(index INTEGER PRIMARY KEY, name TEXT UNIQUE, id TEXT UNIQUE)")
         except sqlite3.OperationalError as e:
             self.__db.rollback()
             logging.error('OperationalError({0}): Database rolled back'.format(e))
@@ -72,28 +51,48 @@ class InventoryDatabase(object):
         else:
             self.__db.commit()
             logging.info('Table "inventory" created')
-
-    def read_product(self, code):
+    
+    def __readFromInventory(self, col, filter, value):
         """
-        Read a product from the databased based on a specified code.
+        Read a row from the inventory database by building a query 
+        according to specified values
         
-        @param code: Unique ID of the product to be read
-        @return:     Return the latest product selected from the database
-                     according to the specified filter.
+        @param col:    Column to read from
+        @param filter: Column to filter results by
+        @param value:  Actual value to filter by
+        @return:       Returns the row that meets the specified query
         """
         cursor = self.__db.cursor()
-        cursor.execute("SELECT product FROM inventory WHERE code=?", (code,))
-        record = cursor.fetchone()
-        if not record:
-            msg = "Product with ID {0} non-existent".format(code)
-            raise RecordDoesNotExistError(msg)
-        return record
-    
-    def insert_product(self, product):
+        query = "SELECT {0} FROM inventory WHERE {2}=?".format(col, filter)
+        cursor.execute(query, (value,))
+        row = cursor.fetchone()
+        return row
+
+    def readProductName(self, ID):
         """
-        Insert a product into the database
+        Read a product name from the database based on a specified ID.
         
-        @param product:  Product to be inserted into the database
+        @param ID: Unique ID of the product to be read
+        @return:   Return the latest product name selected from the database
+                   according to the specified filter.
+        """
+        return self.__readFromInventory('name', 'id', ID)
+        
+    def readID(self, name):
+        """
+        Read an ID from the database based on a specified product name.
+        
+        @param product: Name of the product whose ID is to be read
+        @return:        Return the latest ID selected from the database
+                        according to the specified filter.
+        """
+        return self.__readFromInventory('code', 'name', name)
+    
+    def insertProduct(self, product):
+        """
+        Insert a product (tuple of name and ID) into the database
+        
+        @param product:  The product to be inserted in the inventory
         """
         cursor = self.__db.cursor()
         try:
