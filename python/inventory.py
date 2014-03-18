d527304f4a39470cb5443da7679fe2f25d2d0b12
Steven Briggs <gshbriggs@gmail.com>
2014-03-17 23:23:11 -0400

    Updated UI to be friendlier.
    Added several prompts and messages to improve the user experience. The
    model of the program is essentially complete (might go back and factor a
    Tracker class out). It still does not deal with certain Unicode
    characters very well, but it can handle most. Next goal is to work on an
    actual GUI.

--- python/inventory.py
+++ python/inventory.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 __author__  = 'Steven Briggs'
 __version__ = '2014.03.15'
 
@@ -6,6 +9,7 @@ import logging
 
 DB_NAME = 'inventory.sqlite3'
 
+# Create a file to log information about database operations
 logging.basicConfig(
      filename='inventory.log',
      level=logging.INFO,
@@ -42,13 +46,15 @@ class InventoryDatabase(object):
     def createInventory(self):
         """
         Create a table in the database called 'inventory' with the columns
-        index (primary key), name, and ID.
+        id (primary key), name, and code.
         """
+        query = 'CREATE TABLE inventory(id INTEGER PRIMARY KEY, \
+            name TEXT UNIQUE, code TEXT UNIQUE)'
         try:
-            self.__db.execute("CREATE TABLE inventory(id INTEGER PRIMARY KEY, name TEXT UNIQUE, code TEXT UNIQUE)")
+            self.__db.execute(query)
         except sqlite3.OperationalError as e:
             self.__db.rollback()
-            logging.error('OperationalError({0}): Database rolled back'.format(e))
+            logging.error('OperationalError({0}): Rolled back'.format(e))
         else:
             self.__db.commit()
             logging.info('Table "inventory" created')
@@ -64,7 +70,7 @@ class InventoryDatabase(object):
         @return:       Returns the row that meets the specified query
         """
         cursor = self.__db.cursor()
-        query = "SELECT {0} FROM inventory WHERE {1}=?".format(col, filter)
+        query = 'SELECT {0} FROM inventory WHERE {1}=?'.format(col, filter)
         cursor.execute(query, (value,))
         row = cursor.fetchone()
         return row
@@ -96,12 +102,24 @@ class InventoryDatabase(object):
         @param product:  The product to be inserted in the inventory
         """
         cursor = self.__db.cursor()
+        query = 'INSERT INTO inventory(name, code) VALUES(?, ?)'
         try:
-            cursor.execute("INSERT INTO inventory(name, code) VALUES(?, ?)", product)
+            cursor.execute(query, product)
         except sqlite3.IntegrityError as e:
             self.__db.rollback()
-            logging.error('IntegrityError({0}): Database rolled back'.format(e))
+            logging.error('IntegrityError({0}): Rolled back'.format(e))
             raise
         else:
             self.__db.commit()
-            logging.info('{0} with ID {1} inserted at index {2}'.format(product[0], product[1], cursor.lastrowid))
+            logging.info('{0} with ID {1} inserted at \
+                index {2}'.format(product[0], product[1], cursor.lastrowid))
+    
+    def getNumProducts(self):
+        """
+        Return the number of products currently in the inventory.
+        
+        @return:  Number of products currently in the inventory.
+        """
+        cursor = self.__db.cursor()
+        cursor.execute('SELECT max(id) FROM inventory')
+        return cursor.fetchone()[0]
--- python/main.py
+++ python/main.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 __author__  = 'Steven Briggs'
 __version__ = '2014.03.15'
 
 import inventory
 from inventory import InventoryDatabase
-import codecs
 import string
 import random
 
@@ -27,44 +29,89 @@ def findFile(prompt):
     @return:  Return a file object once it is successfully opened
     """
     file = None
+    # Continue to prompt for a valid file until the user provides one
     while not file:
-        filename = raw_input(prompt)
+        filename = raw_input(prompt).strip()
         try:
-            file = codecs.open(filename, 'rU')
-        except FileNotFoundError as e:
-            print 'File {0} not found.'.format(file)
+            file = open(filename, 'rU')
+        except IOError:
+            print 'File {0} not found.'.format(filename)
         else:
             return file
- 
-def main():
-    print "Welcome to the Inventory Tracker."
+
+def insertProducts(productList, theInventory):
+    """
+    Insert all products (product name, ID) into the database acting as
+    an inventory
     
-    productList = findFile("Enter a file with products to track: ")
-    theInventory = InventoryDatabase()
-    
-    for name in productList:
-        productNameExists = theInventory.readID(name)
+    @param productList:  File containing a list of product names
+    @param theInventory: InventoryDatabase where products are stored
+    """
+    print 'Adding products to the inventory...'
+    numAdded = 0
+    for productName in productList:
+        productNameExists = theInventory.readID(productName)
+        # Determine if the product name provided already exists within
+        # the specified inventory. If it does, then stop attempting to
+        # insert this product and move to the next one.
         if productNameExists:
             continue
             
         ID = ''
         productIDExists = True
+        # Since we know the product is not within the inventory, 
+        # we must continue generating IDs for it until we find a unique one.
         while productIDExists:
             ID = generateID()
             productIDExists = theInventory.readProductName(ID)
         
-        theInventory.insertProduct((name, ID))
+        theInventory.insertProduct((productName, ID))
+        numAdded = numAdded + 1
     
+    print '{0} products added to the inventory'.format(numAdded)
+    productList.close()    
+            
+def main():
+    """
+    Run the Inventory Tracker program
+    """
+    print 'Welcome to the Inventory Tracker.'
+    
+    theInventory = InventoryDatabase()
+    numProducts = theInventory.getNumProducts()
+    
+    if numProducts > 0:
+        print '{0} products in the inventory.'.format(numProducts)
+    else:
+        print '0 products in the inventory.'
+    
+    prompt = 'Provide a file to add products to the inventory? (Y/N) '
+    getFile = raw_input(prompt).strip().lower()
+    
+    if getFile == 'y':
+        # Prompt the user for a file of products and then open it
+        productList = findFile('Enter a file to add products to the inventory: ')
+        # Insert all the products found in productList into the inventory
+        insertProducts(productList, theInventory)
+    
+    # Continually prompt the user to enter an ID for a product.
+    # If the ID is valid, then print the corresponding product name.
     finished = False
     while not finished:
-        ID = raw_input('Enter an ID to retrieve the corresponding product: ')
+        ID = raw_input('Enter an ID to retrieve a product: ').strip()
         productName = theInventory.readProductName(ID)
         if productName:
+            productName = productName[0].rstrip('\n')
             print productName
         else:
-            print 'Invalid ID. A product with ID {0} does not exist.'.format(ID)
-    
+            print 'Invalid ID. A product with ID {0} does not exist'.format(ID)
+        # Check if the user is ready to exit or not
+        decision = raw_input('Exit? (Y/N) ').strip().lower()
+        if decision == 'y':
+            finished = True
+            
     theInventory.closeConnection()
+    exit(0)
             
 if __name__ == '__main__':
     main()
